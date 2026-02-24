ed4b4030e90e0c74b840d8e1cd138bfbe80b9314
Your Actual Name <youremail@example.com>
2026-02-24 14:23:32 +0100

    feat: integrate marketplace and CTA sections into landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import { OurMarketplace } from "@/components/our-marketplace";
+import { LookingToDoMore } from "@/components/looking-to-do-more";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen  ">
+    <div className="font-sans min-h-screen">
       <nav className="bg-amber-400">Nav</nav>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row bg-amber-800 flex-1 w-full">
-          <header className="row-start-1">Body</header>
+      <main className="flex flex-col">
+        <div className="flex gap-4 items-center flex-col sm:flex-row bg-amber-800 w-full py-8">
+          <header className="container mx-auto px-4">Body</header>
         </div>
+        
+        <OurMarketplace />
+        <LookingToDoMore />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap bg-green-700">
-        Footer
+      <footer className="flex gap-[24px] flex-wrap bg-green-700 py-8">
+        <div className="container mx-auto px-4">Footer</div>
       </footer>
     </div>
   );
