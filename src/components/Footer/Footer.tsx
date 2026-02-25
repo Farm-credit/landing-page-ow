6eef79f5a01baa881d817c5012824891002b09b5
ojara <ojarafavour@yahoo.com>
2026-02-25 04:38:15 +0100

    feat:(footer):mobile responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer() {
     <footer className=" bg-white min-h-screen">
        {/* rounded-t-[50px] md:rounded-tl-[180px] lg:rounded-tl-[150px] */}
 
-        <div className="min-h-screen px-6 sm:px-30 pt-40 pb-10 bg-black text-white mt-20   rounded-tl-[55%] rounded-tr-[55%] md:rounded-tl-[40%] md:rounded-tr-[40%] lg:rounded-tl-[30%] lg:rounded-tr-[30%]">
+        <div className="min-h-screen px-6 sm:px-30 pt-40 pb-10 bg-black text-white md:mt-20   rounded-tl-[25%] rounded-tr-[25%] md:rounded-tl-[40%] md:rounded-tr-[40%] lg:rounded-tl-[30%] lg:rounded-tr-[30%]">
         <div className="grid grid-cols-1 md:grid-cols-[1.2fr_1.5fr] gap-10">
           <div>
             <h2 className="text-2xl font-bold">
@@ -45,11 +45,11 @@ export default function Footer() {
             </h2>
           </div>
 
-          <div className="flex justify-between">
+          <div className="flex flex-col justify-between gap-y-8 md:flex-row">
             {/* About */}
             <nav aria-label="About">
               <h3 className="font-semibold mb-4 text-xl">About</h3>
-              <ul className="flex flex-col gap-y-7">
+              <ul className="flex flex-col gap-y-6">
                 {aboutLinks.map(({ label, href }) => (
                   <FooterLink key={label} href={href}>
                     {label}
@@ -61,7 +61,7 @@ export default function Footer() {
             {/* Resources */}
             <nav aria-label="Resources">
               <h3 className="font-semibold text-lg mb-6">Resources</h3>
-              <ul className="flex flex-col gap-y-7">
+              <ul className="flex flex-col gap-y-6">
                 {resources.map(({ label, href }) => (
                   <FooterLink key={label} href={href}>
                     {label}
@@ -74,7 +74,7 @@ export default function Footer() {
             <div>
               <h3 className="font-semibold mb-4">Community</h3>
 
-              <div className="flex flex-col gap-y-7">
+              <div className="flex flex-col gap-y-6">
                 {SOCIAL_LINKS.map(({ label, href }) => (
                   <SocialIcon key={label} href={href} label={label} />
                 ))}
@@ -86,7 +86,7 @@ export default function Footer() {
         <div className="border-t border-gray-800 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center gap-4 text-sm text-gray-500">
           <div className="flex flex-wrap gap-4">
             {LEGAL_LINKS.map((label) => (
-              <ul className="flex items-center gap-5" key={label.label}>
+              <ul className="flex items-center justify-center gap-5" key={label.label}>
                 <FooterLink key={label.label} href={label.href}>
                   {label.label}
                 </FooterLink>
